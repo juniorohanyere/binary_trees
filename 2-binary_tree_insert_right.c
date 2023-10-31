44a09990a04a2b04587467c2c0706cad40a87161
Twin J <junohanyere@gmail.com>
2023-10-31 14:33:22 +0100

    resolved betty coding style errors

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,8 +3,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - function that inserts a node as the right-child of
- *			      another node
+ * binary_tree_insert_right - function that inserts a node as the right-child
+ *			      of another node
  *
  * @parent: pointer to the node to insert the right-child in
  * @value: value to store in the new node
